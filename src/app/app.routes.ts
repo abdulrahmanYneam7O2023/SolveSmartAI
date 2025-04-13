0fce3f4255be158759a0fb85927ec9dc9ac6dc04
ABDO NEAM <abdulrahmanneam@gmail.com>
2025-04-14 01:00:23 +0200

    add Not Found Page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { SigninComponent } from './auth/signin/signin.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './Errors/not-found/not-found.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   {path: 'home', component: HomeComponent },
   { path: 'signin', component: SigninComponent },
   { path: 'signup', component: SignupComponent },
+  { path: '**', component: NotFoundComponent }
 ];
