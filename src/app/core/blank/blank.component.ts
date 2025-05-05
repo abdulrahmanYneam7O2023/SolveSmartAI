c131e180db8660682210451585850a2ffcecdbf4
Heba Ahmed <hebaahmed199717@gmail.com>
2025-05-05 23:45:14 +0300

    Chatbot Icon

--- src/app/core/blank/blank.component.ts
+++ src/app/core/blank/blank.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../shared/header/header.component";
 import { RouterOutlet } from '@angular/router';
+import { ChatbotComponent } from '../../chatbot/chatbot.component';
 
 @Component({
   selector: 'app-blank',
-  imports: [HeaderComponent ,RouterOutlet],
+  imports: [HeaderComponent ,RouterOutlet, ChatbotComponent],
   templateUrl: './blank.component.html',
   styleUrl: './blank.component.css'
 })
